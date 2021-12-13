870d194d2bc84b6b490a3dc35f6d9ca531db0290
reactmore <reactmorecom@gmail.com>
2021-12-13 15:59:01 +0700

    Blank Page For Template

---
+++ app/Views/admin/blank.php
@@ -0,0 +1,52 @@
+<?php echo $this->extend('admin/includes/_layout_view') ?>
+
+<?php echo $this->section('content') ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"><?php echo $title ?></h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <?php if ($title === 'Dashboard') : ?>
+                            <li class="breadcrumb-item active"><a href="<?php admin_url() ?>">/</a></li>
+                        <?php else :  ?>
+                            <li class="breadcrumb-item"><a href="<?php admin_url() ?>"><?php echo trans('dashboard') ?></a></li>
+                            <li class="breadcrumb-item active"><?php echo $title ?></li>
+                        <?php endif  ?>
+
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Main row -->
+            <div class="row">
+                <div class="col-lg-12 col-xl-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Header
+                        </div>
+                        <div class="card-body">
+                            Awesome Content
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php echo $this->endSection() ?>
