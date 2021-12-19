f693a69482d887d9df59d64cb0e91994d64f14b6
reactmore <reactmorecom@gmail.com>
2021-12-19 21:03:02 +0700

    Remove SwiftMailer From Gitignore

---
+++ app/ThirdParty/swiftmailer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9e7ea734aac26bb01b0ada8d92a4dfa2edd1622e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9e7ea734aac26bb01b0ada8d92a4dfa2edd1622e',
+    ),
+    'swiftmailer/swiftmailer' => 
+    array (
+      'pretty_version' => 'v5.4.12',
+      'version' => '5.4.12.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '181b89f18a90f8925ef805f950d47a7190e9b950',
+    ),
+  ),
+);
